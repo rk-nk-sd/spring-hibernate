626051096c46962fc7fe6e5da76b2b4cc7002950
roman <rk-nk-sd@bk.ru>
2020-12-30 18:00:09 +0700

    без реализации метода поиска пользователя по id автомобиля

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,38 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru", new Car("VAZ 21011",1));
+      userService.add(user1);
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("VAZ 2107",7)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("VAZ 2103",3)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("VAZ 2105",5)));
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println("Model auto = "+user.getCar().getModel());
+         System.out.println("Series auto = "+user.getCar().getSeries());
+         System.out.println();
+      }
+
+      context.close();
+   }
+}
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,55 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="cars")
+public class Car {
+//    @OneToOne
+//    @PrimaryKeyJoinColumn
+//    private User user;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String model;
+    private int series;
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+//    public User getUser() {
+//        return user;
+//    }
+//
+//    public void setUser(User user) {
+//        this.user = user;
+//    }
+}
+
